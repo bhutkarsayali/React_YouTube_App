06ca905398b134f2c33552345bcdcd602cb9b448
Sayali <bhutkarsayali@gmail.com>
2025-08-13 00:18:51 +0530

    15.  Routing - useSearch params and dispatching an action to collapse sidebar

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,15 +3,15 @@ import React from "react";
 const VideoCard = ({ info }) => {
   console.log("info", info);
 
-  const { snippet, statistics } = info;
-  const { channelTitle, title, thumbnails } = snippet;
+  // const { snippet, statistics } = info;
+  // const { channelTitle, title, thumbnails } = snippet;
   return (
     <div className="p-2 m-2 shadow-lg w-72">
-      <img alt="thumbnail" src={thumbnails.medium.url} className="rounded-lg" />
+      <img alt="thumbnail" src={info?.snippet?.thumbnails?.medium?.url} className="rounded-lg" />
       <ul>
-        <li className="font-bold">{title}</li>
-        <li className="text-gray-500 pt-2">{channelTitle}</li>
-        <li className="text-gray-500">Views {statistics.commentCount} </li>
+        <li className="font-bold">{info?.snippet?.title}</li>
+        <li className="text-gray-500 pt-2">{info?.snippet?.infochannelTitle}</li>
+        <li className="text-gray-500">Views {info?.statistics?.commentCount} </li>
       </ul>
     </div>
   );
--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
-  return (
-    <div>WatchPage</div>
-  )
-}
+  // const params = useParams();
+  // console.log('params', params);
+  // here above params will come empty, we are using search params here not normal params
 
-export default WatchPage
+  //hence using below hook (check query params in react dom documentation)
+  const [searchParams] = useSearchParams();
+  console.log("searchParams", searchParams.get("v"));
+
+  const dispatch = useDispatch();
+  //As soon as this componnent loads we need to hide sidebar by default hence using it in useEffect hook
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
+  return (
+    <div className="m-5 p-5">
+      <iframe
+        width="1200"
+        height="600"
+        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default WatchPage;
