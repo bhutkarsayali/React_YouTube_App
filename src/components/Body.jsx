4d6cd783cb2bff47f8e041b334adddc9f1252e0f
Sayali <bhutkarsayali@gmail.com>
2025-08-13 00:17:22 +0530

    13.  Routing - using outlet for chnaging route components and for fixed components

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Sidebar from "./Sidebar";
-import MainContainer from "./MainContainer";
+// import MainContainer from "./MainContainer";
+// import WatchPage from "./WatchPage";
+import { Outlet } from "react-router-dom";
 
 const Body = () => {
   return (
     <div className="grid grid-flow-col !h-[calc(100vh - 120px)] overflow-auto flex justify-between">
       <Sidebar />
-      <MainContainer />
+      <Outlet />
+        {/* <MainContainer />
+        <WatchPage /> */}
     </div>
   );
 };
