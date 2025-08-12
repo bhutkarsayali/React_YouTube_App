e2084b7eca0eea63ef4d26ccf73c572520f34939
Sayali <bhutkarsayali@gmail.com>
2025-08-12 18:08:58 +0530

    9. Subscribing to the store using useSelector and subscribint to only s[pecific portion of the store and show-hide sidebar using data coming from redux store

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
+  // Early returrn pattern
+  if (!isMenuOpen) {
+    return null;
+  }
   return (
     <div className="col-span-1 h-screen bg-gray-800 text-white p-4 shadow-2xl">
-    <ul>
+      <ul>
         <li>Home</li>
         <li>Shorts</li>
         <li>Videos</li>
